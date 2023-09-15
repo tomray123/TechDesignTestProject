f5b66781eaac64af2b3d40caf67356cfd9dba57d
tomray123 <Tomray7@yandex.ru>
2023-09-15 21:14:00 +0300

    Create Ui manager script for controlling UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button nextLevelButton;
+    private int currentSceneIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (nextLevelButton != null)
+        {
+            nextLevelButton.enabled = false;
+        }
+        else
+        {
+            Debug.LogWarning("Missing Next Level Button!");
+            return;
+        }
+
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Enables next level button
+    public void EnableNextLevelButton()
+    {
+        if (nextLevelButton != null)
+        {
+            nextLevelButton.enabled = true;
+        }
+        else
+        {
+            Debug.LogWarning("Missing Next Level Button!");
+            return;
+        }
+    }
+
+    public void LoadNextScene()
+    {
+        int totalScenesNumber = SceneManager.sceneCountInBuildSettings;
+        if (currentSceneIndex == totalScenesNumber - 1)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+    }
+}
